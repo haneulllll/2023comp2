9306b2a07566a6a0c2d5ee38e6ee735d75edc0fd
Haneul Lee <haneul217@naver.com>
2023-11-09 11:58:50 +0900

    eul second

--- src/Haneul/haneultest.java
+++ src/Haneul/haneultest.java
@@ -3,5 +3,7 @@ package Haneul;
 public class haneultest {
 	public static void main(String[] args) {
 		System.out.println("Haneul");
+		System.out.println("2nd practice");
+		System.out.println("2nd practice");
 	}
 }
