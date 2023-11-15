4872d1de143d65aa497a8c9a3640e8d16e876014
JunHwan <devjunsday@gmail.com>
2023-11-15 16:32:21 +0900

    test

--- src/View/CalendarFrame.java
+++ src/View/CalendarFrame.java
@@ -3,23 +3,14 @@ import javax.swing.*;
 import java.awt.*;
 import java.time.LocalDate;
 public class CalendarFrame extends JFrame {
-	JPanel Date;
+	JPanel DatePanel;
 	JButton RightButton;
 	JButton LeftButton;
 	JPanel CalendarPanel;
 	JPanel addPanel;
 	public CalendarFrame(LocalDate today) {
+		
 		setSize(1000,1000);
-		JLabel testlabel1 = new JLabel("North label");
-		JLabel testlabel2 = new JLabel("South label");
-		JLabel testlabel3 = new JLabel("West label");
-		JLabel testlabel4 = new JLabel("East label");
-		JLabel testlabel5 = new JLabel("Center label");
-		add(testlabel1,"North");
-		add(testlabel2,"South");
-		add(testlabel3,"West");
-		add(testlabel4,"East");
-		add(testlabel5,"Center");
 		setVisible(true);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
